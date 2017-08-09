3900a0bf788e97c9636659ff9195315a33471199
Boriphuth Saensukphattraka <boriphuth.saensukphattraka@live.com>
2017-08-09 15:55:16 +0700

    First commit.

---
+++ StrongerPasswordsApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StrongerPasswordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var hash = PasswordHash.CreateHash("password1234");
+            PasswordHash.ValidatePassword("password1234", hash);
+
+            stopWatch.Stop();
+
+            Console.WriteLine("The hash was {0}", hash);
+            Console.WriteLine("It took {0} ms to hash and validate a password.", stopWatch.Elapsed.TotalMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
